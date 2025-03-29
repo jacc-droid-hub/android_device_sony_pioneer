f66d1a551ea7aa7b3cdaed60e2204f86544b80f6
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2025-03-28 22:48:38 -0500

    Adjust for building RR Q

--- rr_pioneer.mk
+++ rr_pioneer.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/sony/pioneer/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_pioneer
+PRODUCT_NAME := rr_pioneer
 PRODUCT_DEVICE := pioneer
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := Xperia XA2
